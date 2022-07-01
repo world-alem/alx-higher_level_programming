05e75545e6ca5a5e812c98db918556797c55c87a
Biruk Ephrem <birukeph@gmail.com>
2022-07-01 21:51:16 +0300

    doing task 1

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    Integer addition
+"""
+
+
+def add_integer(a, b=98):
+    """Adds two integers"""
+
+    if type(a) not in [int, float]:
+        raise TypeError('TypeError: a must be an integer')
+    if type(a) not in [int, float]:
+        raise TypeError('TypeError: b must be an integer')
+
+    return int(a) + int(b)
