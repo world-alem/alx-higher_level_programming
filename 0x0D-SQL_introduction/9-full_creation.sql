3a17df6a436fcf3e3df20080d0e2bcae9c56f02c
Alem Haile <wellma043@gmail.com>
2022-08-10 21:35:52 +0300

    did task

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,9 @@
 -- table with multiple row
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+    name VARCHAR(256),
+    score INT
+);
 INSERT INTO second_table (id, name, score) VALUES
 (1, "John", 10),
 (2, "Alex", 3),
