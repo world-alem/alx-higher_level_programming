a61b1a00bed7d895e5d52d09978daca6c1d70b97
Alem Haile <wellma043@gmail.com>
2022-08-10 22:58:03 +0300

    did task

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,14 @@
+-- convert to UTF8(utf8mb4, collate utfmb4_unicode_ci)
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE first_table
+CONVERT TO
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE first_table
+MODIFY name VARCHAR(256)
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
