7e7267800649b2a6dde41ac579ffd55e63f712dd
Alem Haile <wellma0043@gmail.com>
2022-11-21 20:49:31 +0300

    python

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This is a square class project"""
+
+
+class Square:
+    """the square class"""
+
+    def __init__(self, size):
+        """the init method"""
+        self.__size = size
