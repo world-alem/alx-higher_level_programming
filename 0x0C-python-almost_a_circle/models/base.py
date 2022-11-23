dd44168c72f087bdf2af86ab8978ef6b6692fa04
Alem Haile <wellma0043@gmail.com>
2022-11-23 15:13:14 +0300

    python test

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""this should ba a documentation"""
+
+
+class Base:
+    """base class"""
+
+    __nb_objects = 0
+
+    def __init(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            self.__nb_objects += 1
+            self.id = self.__nb_objects
